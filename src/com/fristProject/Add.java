d8163e1b86e42b151af8a2a5037241066caaab63
ThummanapelliPranav <pranavchintu125@gmail.com>
2023-05-31 14:57:33 +0530

    addition

---
+++ src/com/fristProject/Add.java
@@ -0,0 +1,14 @@
+package com.fristProject;
+
+public class Add {
+
+	public static void main(String[] args) {
+    int num1=10;
+    int num2=20;
+    int result;
+    result=num1+num2;
+    System.out.println("Additon of two nums :" + result);
+  
+	}
+
+}
